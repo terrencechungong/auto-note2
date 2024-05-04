831791c5146a38b81f6a47ebd7bc0d0cc94e63f0
unknown <terrencechungong@gmail.com>
2024-05-04 14:33:44 -0400

    login page

--- src/frontend/SignUp.js
+++ src/frontend/SignUp.js
@@ -1,120 +1,133 @@
-import React from 'react';
-import { auth, database } from '../backend/firebase_file'
-import { ref, set, child, update, remove, orderByChild, equalTo, query, get } from 'firebase/database';
-import { User } from '../backend/domain/Student';
-import { Validation } from '../backend/Validation';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockPersonIcon from '@mui/icons-material/LockPerson';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-//turn error messages into messages under their boxes on the screen. Not alerts
-//TODO: Add options to sign up with third party sites
-//Turn to class based function. Add logic to display error messages.
-//allow users to add a profile picture
-
-
-const callRegister = () => {
-  Validation.register(
-    document.getElementById("firstName").value,
-    document.getElementById("lastName").value,
-    document.getElementById("username").value,
-    document.getElementById("emailAddress").value,
-    document.getElementById("phoneNumber").value,
-    document.getElementById("password1").value,
-    document.getElementById("password2").value
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
+// TODO remove, this demo shouldn't need to reset the theme.
 
-const Signup = () => {
+const defaultTheme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <div class="login-css">
-      <section class="vh-100">
-        <div class="container py-5 h-100">
-          <div class="row justify-content-center align-items-center h-100">
-            <div class="col-12 col-lg-9 col-xl-7">
-              <div class="card bg-dark text-white card-registration" style={{ borderRadius: "15px" }}>
-                <div class="card-body p-4 p-md-5">
-                  <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Sign Up</h3>
-                  <p>Sign up for a chance to win MVP at your next pick up game!</p>
-
-
-                  <div class="row">
-                    <div class="col-md-6 mb-4">
-
-                      <div class="form-outline">
-                        <input type="text" id="firstName" class="transp form-control-lg" placeholder="First Name" />
-                      </div>
-
-                    </div>
-                    <div class="col-md-6 mb-4">
-
-                      <div class="form-outline">
-                        <input type="text" id="lastName" class="transp form-control-lg" placeholder='Last Name' />
-                      </div>
-
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-md-6 mb-4 d-flex align-items-center">
-
-                      <div class="form-outline">
-                        <input type="text" id="username" class="transp form-control-lg" placeholder='Username' />
-                      </div>
-
-                    </div>
-                    <div class="col-md-6 mb-4">
-
-                      <div class="form-outline">
-                        <input type="email" id="emailAddress" class="transp form-control-lg" placeholder='Email' />
-                      </div>
-
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-md-6 mb-4 pb-2">
-
-                      <div class="form-outline">
-                        <input type="tel" id="phoneNumber" class="transp form-control-lg" placeholder='Phone number' />
-                      </div>
-
-                    </div>
-                    <div class="col-md-6 mb-4 pb-2">
-
-                      <div class="form-outline">
-                        <input type="password" id="password1" class="transp form-control-lg" placeholder='Password' />
-
-                      </div>
-
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-md-6 mb-4">
-
-                      <div class="form-outline">
-                        <input type="password" id="password2" class="transp form-control-lg" placeholder='Re-enter password' />
-                      </div>
-
-                    </div>
-
-                  </div>
-
-                  <div class="mt-4 pt-2">
-                    <button class="btn btn-outline-light btn-lg px-5" onClick={callRegister}>Sign Up</button>
-                  </div>
-                  <br />
-                  <div>
-                    <p class="mb-0">Already have an account? <a href="/login" class="text-white-50 fw-bold">Sign In</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockPersonIcon />
+          </Avatar> */}
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="log-in" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default Signup;
--- src/frontend/reusables/top-words-home-page.js
+++ src/frontend/reusables/top-words-home-page.js
@@ -1,8 +1,8 @@
 let app_name = { fontSize: 60, marginRight: 200, marginLeft: 100, fontWeight: 100, marginTop: -28 };
 
-let sign_up_style = { fontSize: 30, marginRight: 40, fontWeight: 600, backgroundColor: 'white', borderRadius: 20, width: '70%', textAlign: 'center', transition: 'background-color 0.3s ease-in-out', paddingBottom: 2 };
+let sign_up_style = { fontSize: 30, marginRight: 40, fontWeight: 600, backgroundColor: 'white', borderRadius: 20, width: '70%', textAlign: 'center', transition: 'background-color 0.3s ease-in-out', paddingBottom: 2, textDecoration: 'none',color:'black' };
 
-let log_in_style = { fontSize: 30, marginRight: 40, fontWeight: 600, width: '60%', textAlign: 'center', alignSelf: 'center' };
+let log_in_style = { fontSize: 30, marginRight: 40, fontWeight: 600, width: '60%', textAlign: 'center', alignSelf: 'center',textDecoration: 'none',color:'black' };
 
 export const TopWords = () => {
     return (<div className="side-by-side-container" style={{ paddingTop: 60 }}>
@@ -14,14 +14,14 @@ export const TopWords = () => {
 
 export const AuthWords = () => {
     return (<div className="side-by-side-container" style={{ paddingTop: 60, position: 'absolute', right: 40, width: '10vw' }}>
-        <p style={log_in_style}>Log In</p>
-        <p onMouseLeave={() => { }}
+        <a style={log_in_style} href='/log-in'>Log In</a>
+        <a onMouseLeave={() => { }}
             onMouseOver={(e) => {
                 e.currentTarget.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
             }}
             onMouseOut={(e) => {
                 e.currentTarget.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-            }} style={sign_up_style}>Sign Up</p>
+            }} style={sign_up_style} href='/sign-up' >Sign Up</a>
     </div>)
 }
 
